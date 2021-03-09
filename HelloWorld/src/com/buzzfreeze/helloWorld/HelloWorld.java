82da91e73a4c4770105eff41c4ffe5685de6b5e3
arada88 <arada88@hotmail.com>
2021-03-09 16:39:20 +0700

    java1variable

---
+++ HelloWorld/src/com/buzzfreeze/helloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.buzzfreeze.helloWorld;
+
+public class HelloWorld {
+    public static void main(String [] args) {
+    	System.out.println("Hello World");
+    	//System.out.printIn("Good Bye");
+    	
+    	/*
+    	System.out.printIn("Monday Morning");
+    	System.out.printIn("Friday Evening");
+    	*/
+    }
+}
---
+++ HelloWorld/src/com/buzzfreeze/helloWorld/Variable.java
@@ -0,0 +1,22 @@
+package com.buzzfreeze.helloWorld;
+
+public class Variable {
+     public static void main(String[] args) {
+    	 /*Data type: integer
+    	 Variable name: mylnt
+    	 */
+    	 int mylnt;
+    	 //Assign value to mylnt = 20;
+    	 mylnt = 20;
+    	 boolean isSingle = true;
+    	 String greeting = "Hi";
+    	 float firstNumber = 2.42f;
+    	 double secondNumber = 102.34d;
+    	 
+    	 System.out.println("mylnt value: " + mylnt);
+    	 System.out.println("isSingle value: " + isSingle);
+    	 System.out.println("greeting value: " + greeting);
+    	 System.out.println("firstNumber value: " + firstNumber);
+    	 System.out.println("secongNumber value: " + secondNumber);
+     }
+}
