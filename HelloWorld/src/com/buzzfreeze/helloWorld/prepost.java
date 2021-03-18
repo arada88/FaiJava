fbe8c70584e932b574549aec8d758d65d1646676
arada88 <arada88@hotmail.com>
2021-03-18 17:04:18 +0700

    ep3
    Change-Id: I00572735e74761517c563d8ba0094856a4559e7d

---
+++ HelloWorld/src/com/buzzfreeze/helloWorld/prepost.java
@@ -0,0 +1,30 @@
+package com.buzzfreeze.helloWorld;
+
+public class prepost {
+	public static void main( String [] args) {
+		int i = 20;
+		i++;
+		i++;
+		i++;
+		i++;
+		i++;
+		System.out.println(i);
+		--i;
+		--i;
+		--i;
+		--i;
+		--i;
+		System.out.println(i);
+		
+		float num1 = 3.14f;
+		float num2 = 6.34f;
+		if ((num1 == 3.14f) && ( num2 == 6.34f));
+		System.out.println("num1 is 3.14f AND num2 is 6.34f");
+		
+		char name = 'M';
+		char surname = 'D';
+		if ((name == 'F') || (surname == 'D'));
+		System.out.println("true");
+		
+		}
+}
