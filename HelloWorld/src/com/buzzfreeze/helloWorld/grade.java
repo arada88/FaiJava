255c086afa94d4a793b80ed66bfbb1d4cd45a6e9
arada88 <arada88@hotmail.com>
2021-03-18 19:09:43 +0700

    lab4.3

---
+++ HelloWorld/src/com/buzzfreeze/helloWorld/grade.java
@@ -0,0 +1,21 @@
+package com.buzzfreeze.helloWorld;
+
+public class grade {
+     public static void main(String[] args) {
+    	 int score = 60;
+    	 switch (score) {
+    	 case 80:
+    		 System.out.println("A");break;
+    	 case 70:
+    		 System.out.println("B");break;
+    	 case 60:
+    		 System.out.println("C");break;
+    	 case 50:
+    		 System.out.println("D");break;
+    	 case 40:
+    		 System.out.println("F");break;
+    	default:
+    		System.out.println("E");
+    	 }
+     }
+}
