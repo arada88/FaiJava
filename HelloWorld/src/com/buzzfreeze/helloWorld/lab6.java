e8fb10b8b9e2a94d47823e69e9bbe8da4578514e
arada88 <arada88@hotmail.com>
2021-04-07 20:58:42 +0700

    lab6

---
+++ HelloWorld/src/com/buzzfreeze/helloWorld/lab6.java
@@ -0,0 +1,9 @@
+package com.buzzfreeze.helloWorld;
+
+public class lab6 {
+    public static void main(String[] args) {
+    	char[] myChar = {'G','o','o','d',' ','M','o','r','n','i','n','g'};
+    	String myWord = new String(myChar);
+    	System.out.println(myWord);
+    }
+}
