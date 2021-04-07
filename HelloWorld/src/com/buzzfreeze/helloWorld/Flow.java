2c9e35385e2895762a00997d3c48dcea376d75a9
arada88 <arada88@hotmail.com>
2021-04-07 20:47:57 +0700

    lab5

---
+++ HelloWorld/src/com/buzzfreeze/helloWorld/Flow.java
@@ -0,0 +1,11 @@
+package com.buzzfreeze.helloWorld;
+
+public class Flow {
+    public static void main(String[] args) {
+    	int result = plus(7,10);
+    	System.out.println("This is the result "+ result);
+    }
+    public static int plus(int a, int b) {
+    	return a + b;
+    }
+}
