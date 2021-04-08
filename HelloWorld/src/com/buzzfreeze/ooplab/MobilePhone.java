7bc76d8491ac18fc80bed08b8ac0356c0fdcf049
arada88 <arada88@hotmail.com>
2021-04-08 20:00:34 +0700

    lab9.2

--- HelloWorld/src/com/buzzfreeze/ooplab/MobilePhone.java
+++ HelloWorld/src/com/buzzfreeze/ooplab/MobilePhone.java
@@ -1,5 +1,29 @@
 package com.buzzfreeze.ooplab;
 
 public class MobilePhone {
-
+	public void feature() {
+		PhoneCall();
+		SMS();
+		InternetSurfing();
+    public void iphone() {
+    	AppStore();
+    	public void iphoneX {
+    		FaceID();
+    	}
+    	public void iphone8 {
+    		TouchID();
+    	}
+    }
+    public void Android() {
+    	Google Play();
+    	public void Samsung Galaxy {
+    		UseGearVR();
+    		TransformToPC();
+    		public void Samsung Galaxy Note8 {
+    			UsePen();
+    		}
+    		public void Samsung Galaxy S8
+    	}
+    }
+		}
 }
